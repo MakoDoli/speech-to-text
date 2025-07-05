7c896ce924a01dc230fc6f1f3861d3fa010bafe5
MakoDoli <makodolidze@hotmail.com>
2025-07-06 01:43:48 +0400

    update prisma instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from "@/generated/prisma";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
