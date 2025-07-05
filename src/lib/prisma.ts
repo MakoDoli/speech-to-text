97faf978e5b31a05aa2b2a9a22a8f7ec5b8bcd6e
MakoDoli <makodolidze@hotmail.com>
2025-07-06 02:16:47 +0400

    updated prisma instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,21 @@
-import { PrismaClient } from "@/generated/prisma";
+//import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export { prisma };
+// export { prisma };
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
