92e3dc600271a1c24ecc15c454bdfa184ab683cc
MakoDoli <makodolidze@hotmail.com>
2025-07-06 02:30:05 +0400

    update prisma instance binary targets

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "@/generated/prisma";
 //import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@/generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
